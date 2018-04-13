c1b2bee4b47b84c813aa7cd836447a2a9f429004
Paul Mendez <pmendez@ing-mat.udec.cl>
2018-04-13 18:24:23 -0300

    Add Dubiner polynomials gradient

--- polynomials.jl
+++ polynomials.jl
@@ -79,28 +79,65 @@ function dubiner_base2(j::Integer,z,w)
 end
 
 using PolynomialBases
+function eval_Djacobi(x,n::Integer,α, β)
+    T = typeof((2+α+β)*x/2)
+    a = zero(T)
+    if n <= 0;return a;end
+    return (n+α+β+1)/2*jacobi(x,n-1,α+1,β+1)
+end
 """
 eval_dubiner(z,w,j::Integer)
-Compute the dubiner polynomial of degree `j` at point (z,w)
-on the reference triangle ((0,0),(1,0),(0,1)) ?
+Compute the dubiner polynomial of degree `j` at point (x,y)
+on the reference triangle ((0,0),(1,0),(0,1))
 """
-function eval_dubiner(z,w,j::Integer)
-    S=2*w-1
-    R=(2*(1+(2*z-1))/(1-S))-1
+function eval_dubiner(x,y,j::Integer)
+    #check domain
+    @assert ((y>=0)&&(x>=0))&&(1>=x+y) "point not in domain"
+    # Map to reference square
+    ξ=2*x/(1-y)-1
+    η=2*y-1
+    #Compute degrees
     t=-3/2+(1/2)*sqrt(1+8*j)
-    n=((ceil(t)+1)*(ceil(t)+2))/2-j;
-    k=2*n+1;
-    m=ceil(t)-n;
-    @assert ((w>=0)&&(z>=0))&&(1>=z+w) "point not in domain"
-    P=jacobi(R,Int(n),0,0)*jacobi(S, Int(m),k,0)*((1-S)/2)^n
+    n=((ceil(t)+1)*(ceil(t)+2))/2-j
+    k=2*n+1
+    m=ceil(t)-n
+    #Compute Dubiner_nm(ξ, η)
+    P=jacobi(ξ,Int(n),0,0)*jacobi(η, Int(m),k,0)*((1-η)/2)^n
     N=sqrt(2/((2*n+1)*(m+n+1)))
     return (2*P)/N
 end
 
 """
+eval_Ddubiner(z,w,j::Integer)
+Compute the gradient of dubiner polynomial of degree `j` at point (x,y)
+on the reference triangle ((0,0),(1,0),(0,1))
+"""
+function eval_Ddubiner(x,y,j::Integer)
+    #check domain
+    @assert ((y>=0)&&(x>=0))&&(1>=x+y) "point not in domain"
+    # Map to reference square
+    ξ=2*x/(1-y)-1
+    η=2*y-1
+    #Compute degrees
+    t=-3/2+(1/2)*sqrt(1+8*j)
+    n=Int(((ceil(t)+1)*(ceil(t)+2))/2-j)
+    k=2*n+1
+    m=Int(ceil(t)-n)
+    #Compute ∇Dubiner_nm(ξ, η)
+    Dφn = eval_Djacobi(ξ,n,0,0)
+    φn = jacobi(ξ,n,0,0)
+    φm = jacobi(η,m,k,0)
+    Dφm = eval_Djacobi(η,m,k,0)
+    Px=2/(1-η)*Dφn*φm*((1-η)/2)^n
+    N=sqrt(2/((2*n+1)*(m+n+1)))
+    Py=(2*x/(1-y)^2*Dφn*((1-η)/2)^n-n*((1-η)/2)^(n-1)*φn)*φm + 2*φn*((1-η)/2)^n*Dφm
+    return [(4*Px)/N,(2*Py)/N]
+end
+
+"""
 eval_dubiner_ex(z,w,j::Integer)
-Compute the dubiner polynomial of degree `j` at point (z,w)
-on the reference triangle ((0,0),(1,0),(0,1)) ?
+Compute the dubiner polynomial of degree `j` at point (r,s)
+on the reference triangle ((0,0),(1,0),(0,1))
 Use exact functions when available
 """
 function eval_dubiner_ex(r::T,s::T,j::Integer) where{T}
@@ -124,25 +161,47 @@ function eval_dubiner_ex(r::T,s::T,j::Integer) where{T}
     end
 end
 
-#test
-r = 0.5; s=0.5
-p = zeros(15,1)
-p[1] = sqrt(2)
-p[2] = 2*sqrt(3)*(2*r + s - 1)
-p[3] = 2*(3*s - 1)
-p[4] = sqrt(30)*(6*r^2 + 6*r*(s - 1) + s^2 - 2*s + 1)
-p[5] = 3*sqrt(2)*(5*s - 1)*(2*r + s - 1)
-p[6] = sqrt(6)*(10*s^2 - 8*s + 1)
-p[7] = 2*sqrt(14)*(2*r + s - 1)*(10*r^2 + 10*r*(s - 1) + s^2 - 2*s + 1)
-p[8] = 2*sqrt(10)*(7*s - 1)*(6*r^2 + 6*r*(s - 1) + s^2 - 2*s + 1)
-p[9] = 2*sqrt(6)*(21*s^2 - 12*s + 1)*(2*r + s - 1)
-p[10] = 2*sqrt(2)*(35*s^3 - 45*s^2 + 15*s - 1)
-p[11] = 3*sqrt(10)*(70*r^4 + 140*r^3*(s - 1) + 90*r^2*(s^2 - 2*s + 1) + 20*r*(s^3 - 3*s^2 + 3*s - 1) + s^4 - 4*s^3 + 6*s^2 - 4*s + 1)
-p[12] = sqrt(70)*(9*s - 1)*(2*r + s - 1)*(10*r^2 + 10*r*(s - 1) + s^2 - 2*s + 1)
-p[13] = 5*sqrt(2)*(36*s^2 - 16*s + 1)*(6*r^2 + 6*r*(s - 1) + s^2 - 2*s + 1)
-p[14] = sqrt(30)*(84*s^3 - 84*s^2 + 21*s - 1)*(2*r + s - 1)
-p[15] = sqrt(10)*(126*s^4 - 224*s^3 + 126*s^2 - 24*s + 1)
+"""
+eval_Ddubiner_ex(z,w,j::Integer)
+Compute the gradient of dubiner polynomial of degree `j` at point (r,s)
+on the reference triangle ((0,0),(1,0),(0,1))
+Use exact functions when available
+"""
+function eval_Ddubiner_ex(r::T,s::T,j::Integer) where{T}
+    if j == 0; return [zero(T),zero(T)]
+    elseif j == 1; return [zero(T),zero(T)]
+    elseif j == 2; return [4*sqrt(3), 2*sqrt(3)]*one(T)
+    elseif j == 3; return [zero(T), one(T)*6]
+    elseif j == 4; return [12*sqrt(30)*r + 6*sqrt(30)*(s - 1),
+                            6*sqrt(30)*r + 2*sqrt(30)*s - 2*sqrt(30)]
+    elseif j == 5; return [6*sqrt(2)*(5*s - 1),
+                            30*sqrt(2)*r + 30*sqrt(2)*s - 18*sqrt(2)]
+    elseif j == 6; return [zero(T),20*sqrt(6)*s - 8*sqrt(6)]
+    elseif j == 7; return [24*sqrt(14)*(5*r^2 + 5*r*(s - 1) + s^2 - 2*s + 1),
+                            60*sqrt(14)*r^2 + r*(48*sqrt(14)*s - 48*sqrt(14)) + 6*sqrt(14)*s^2 - 12*sqrt(14)*s + 6*sqrt(14)]
+    elseif j == 8; return [12*sqrt(10)*(7*s - 1)*(2*r + s - 1),
+                            84*sqrt(10)*r^2 + r*(168*sqrt(10)*s - 96*sqrt(10)) + 42*sqrt(10)*s^2 - 60*sqrt(10)*s + 18*sqrt(10)]
+    elseif j == 9; return [4*sqrt(6)*(21*s^2 - 12*s + 1),
+                            r*(168*sqrt(6)*s - 48*sqrt(6)) + 126*sqrt(6)*s^2 - 132*sqrt(6)*s + 26*sqrt(6)]
+    elseif j == 10; return [zero(T),210*sqrt(2)*s^2 - 180*sqrt(2)*s + 30*sqrt(2)]
+    elseif j == 11; return [840*sqrt(10)*r^3 + 1260*sqrt(10)*r^2*(s - 1) + 540*sqrt(10)*r*(s^2 - 2*s + 1) + 60*sqrt(10)*(s^3 - 3*s^2 + 3*s - 1),
+                            420*sqrt(10)*r^3 + r^2*(540*sqrt(10)*s - 540*sqrt(10)) + r*(180*sqrt(10)*s^2 - 360*sqrt(10)*s + 180*sqrt(10)) + 12*sqrt(10)*s^3 - 36*sqrt(10)*s^2 + 36*sqrt(10)*s - 12*sqrt(10)]
+    elseif j == 12; return [12*sqrt(70)*(9*s - 1)*(5*r^2 + 5*r*(s - 1) + s^2 - 2*s + 1),
+                            180*sqrt(70)*r^3 + r^2*(540*sqrt(70)*s - 300*sqrt(70)) + r*(324*sqrt(70)*s^2 - 456*sqrt(70)*s + 132*sqrt(70)) + 36*sqrt(70)*s^3 - 84*sqrt(70)*s^2 + 60*sqrt(70)*s - 12*sqrt(70)]
+    elseif j == 13; return [30*sqrt(2)*(36*s^2 - 16*s + 1)*(2*r + s - 1),
+                            r^2*(2160*sqrt(2)*s - 480*sqrt(2)) + r*(3240*sqrt(2)*s^2 - 3120*sqrt(2)*s + 510*sqrt(2)) + 720*sqrt(2)*s^3 - 1320*sqrt(2)*s^2 + 690*sqrt(2)*s - 90*sqrt(2)]
+    elseif j == 14; return [2*sqrt(30)*(84*s^3 - 84*s^2 + 21*s - 1),
+                            r*(504*sqrt(30)*s^2 - 336*sqrt(30)*s + 42*sqrt(30)) + 336*sqrt(30)*s^3 - 504*sqrt(30)*s^2 + 210*sqrt(30)*s - 22*sqrt(30)]
+    elseif j == 15; return [zero(T),504*sqrt(10)*s^3 - 672*sqrt(10)*s^2 + 252*sqrt(10)*s - 24*sqrt(10)]
+    else; return eval_Ddubiner(r,s,j)
+    end
+end
 
+
+#test
+const rr = 0.5
+const ss = 0.5
 for j = 1:15
-    println(eval_dubiner(r,s,j) ≈ p[j])
+    println(eval_dubiner(rr,ss,j) ≈ eval_dubiner_ex(rr,ss,j))
+    println(eval_Ddubiner(rr,ss,j) ≈ eval_Ddubiner_ex(rr,ss,j))
 end
